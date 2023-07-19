3490b034b69151ffb91cd7529581d2e84f35ac6d
DeadBodyJun <630cb@naver.com>
2023-07-19 11:36:40 +0900

    연타
    연타하면 속력붙고 아니면 줄어드는 처리

--- Assets/Script/RaceGameManager.cs
+++ Assets/Script/RaceGameManager.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
+
 public class RaceGameManager: MonoBehaviour
 {
-    public Transform Player;
+    public GameObject Player;
     public float Speed=0f;
     
     private void Update()
     {
         if(Input.GetKeyUp(KeyCode.Space))
         {
+            //연타하면 속력이 붙음
             Speed += 15f;
             Player.GetComponent<Rigidbody>().AddForce(0,0,Speed*Time.deltaTime*50);
+        }
+        else
+        {
+            //연타멈추면 속력이 줄어들음
+            Player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, Speed * Time.deltaTime);
+
+        }
+        //뒤로 안가게 하기위한 처리
+        Speed -= (0.5f*Time.deltaTime)*50;
+        if(Speed <=0.0f)
+        {
+            Speed = 0;
         }
     }
 }
