9ac209c77a962418be45226dc687f17d26a0fc29
DeadBodyJun <630cb@naver.com>
2023-07-20 11:49:24 +0900

    카메라 움직임 및 블러처리 추가
    범서야 재욱아 승원아 너네가 세명이 조절해서 가장 좋거나 아니면 너네가 나눠서 오늘안에 작업해야한다.

--- Assets/Script/RaceGameManager.cs
+++ Assets/Script/RaceGameManager.cs
@@ -9,14 +9,18 @@ using DG.Tweening;
 public class RaceGameManager: MonoBehaviour
 {
     public GameObject Player;
-    public float Speed=0f;
-    
+    public float Speed;
+    private void Start()
+    {
+        Speed = 00f;
+    }
     private void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Space))
+       
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             //연타하면 속력이 붙음
-            Speed += 15f;
+            Speed += 20f;
             Player.GetComponent<Rigidbody>().AddForce(0,0,Speed*Time.deltaTime*50);
         }
         else
@@ -25,8 +29,12 @@ public class RaceGameManager: MonoBehaviour
             Player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, Speed * Time.deltaTime);
 
         }
+        if(Speed >= 800f)
+        {
+            Speed = 799f;
+        }
         //뒤로 안가게 하기위한 처리
-        Speed -= (0.5f*Time.deltaTime)*50;
+        Speed -= (0.5f*Time.deltaTime)*80;
         if(Speed <=0.0f)
         {
             Speed = 0;
--- Assets/Script/cameraMove.cs
+++ Assets/Script/cameraMove.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class cameraMove : MonoBehaviour
 {
+    public RaceGameManager race;
     public Transform player;  // 따라다닐 플레이어의 Transform 컴포넌트
 
-    public Vector3 offset = new Vector3(0f, 5f, -10f);  // 카메라와 플레이어 간의 초기 오프셋
+    public Vector3 offset = new Vector3(0f, 5, -10f);  // 카메라와 플레이어 간의 초기 오프셋
 
-    public float smoothSpeed = 0.5f;  // 카메라 이동 스무딩 속도
-
-    private void LateUpdate()
+    private void Start()
     {
-        // 플레이어 위치에 따라 카메라를 이동시킴
-        Vector3 desiredPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        GameObject obj = GameObject.Find("Slime_01");
+        if (obj != null)
+        {
+            race = obj.GetComponent<RaceGameManager>();
+        }
+
     }
+        void LateUpdate()
+        {
+          // 플레이어 위치에 따라 카메라를 이동시킴
+          transform.position = offset + player.position;
+          if(race.Speed >= 500)
+          {
+            this.gameObject.GetComponent<Camera>().fieldOfView -=1;
+            if(this.gameObject.GetComponent<Camera>().fieldOfView <=45)
+            {
+                this.gameObject.GetComponent<Camera>().fieldOfView = 30;
+            }
+          }
+        
+        }
 }
+/*
+ 카메라 포커스 렌더링하는거 조절하자 애들아 형이 여기다가 하는법 적어둔다
+
+main camera 하이어라키창가서 addComponent에서 camera filter pack이라고 폴더있다 거기서 카메라 효과를 선택해서 조율하낟.
+우리는 카메라 블러가 필요하기떄문에 blur 라는 폴더에서 사용해서 최대한 속도감을 내보자.
+가속했을때 어느정도 race.speed값을 받아서 this.gameObject.GetComponent<너네가 추가한 스크립트>() 해서 여기다가 블러처리 하자
+형이 간단한 예시코드 작성 할태니까 보고 해보렴
+
+if(race.Speed > 500)
+{
+     this.gameObject.GetComponent<Focus>().Eyes = 30;
+}
+이런식으로 너네가 가져오고싶은 값을 가져와서 숫자로 조절하면된다 쉽지?
+하지만 이게 완전 노가다라서 여러명이서 해야 편하다.
+
+이거 블러처리랑 카메라 fov값 건드리는 코드는 형이 위에 작성해놧어 이걸로 조절해봐
+ㅇㅇ
+ */
