b5fdd2be0ed44253db8b6e7a3796b0e9b3b7f1b3
gyshim <gyshim1004@gmail.com>
2023-07-20 23:51:49 +0900

    NPC 추가
    Distance를 이용해서 둘 사이의 거리에 따라 npc의 속도를 변화시켜 밸런스를 조정하고자 했으나 실패함. (Distance값 안 나옴)
    Skill 사용시, Player속도 증가 및 Npc 속도 감소.
    시뮬레이터 재생 시, Npc 오브젝트에 들어있는 스크립트의 체크박스가 해제됨. 체크 시에 정상 작동하나 확인 필요.

--- Assets/Script/RaceGameManager.cs
+++ Assets/Script/RaceGameManager.cs
@@ -6,60 +6,83 @@ using UnityEngine.EventSystems;
 using DG.Tweening;
 using UnityEngine.InputSystem; // 조이스틱 입력값 받기
 
-
-public class RaceGameManager: MonoBehaviour
+public class RaceGameManager : MonoBehaviour
 {
     public GameObject Player;
     public float Speed;
-    private void Start()
+    public float PlayerZ;
+    public float NpcZ;
+    
+
+
+    // 싱글톤 패턴
+    public static RaceGameManager instance;
+
+    // 다른 스크립트에서 playerInput 변수에 접근할 수 있도록 변경합니다.
+    public PlayerInput PlayerInput { get; private set; }
+
+    private void Awake()
     {
-        Speed = 00f;
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        PlayerInput = GetComponent<PlayerInput>(); // PlayerInput 컴포넌트를 가져옵니다.
+        PlayerInput.SwitchCurrentActionMap("Player"); // "Player"는 InputActionAsset에 정의된 이름입니다.
     }
 
-    /*
+    private void Start()
+    {
+        Speed = 0f;
+
+        PlayerZ = Player.transform.position.z; //초기 위치 저장
+        
+    }
+
     private void Update()
     {
+        /*
         if (Input.GetKeyUp(KeyCode.Space))
         {
             //연타하면 속력이 붙음
             Speed += 20f;
-            Player.GetComponent<Rigidbody>().AddForce(0,0,Speed*Time.deltaTime*50);
+            Player.GetComponent<Rigidbody>().AddForce(0, 0, Speed * Time.deltaTime * 50);
         }
         else
         {
             //연타멈추면 속력이 줄어들음
             Player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, Speed * Time.deltaTime);
-
         }
-        if(Speed >= 800f)
+
+        if (Speed >= 800f)
         {
             Speed = 799f;
         }
+
         //뒤로 안가게 하기위한 처리
-        Speed -= (0.5f*Time.deltaTime)*80;
-        if(Speed <=0.0f)
+        Speed -= (0.5f * Time.deltaTime) * 80;
+
+        if (Speed <= 0.0f)
         {
             Speed = 0;
         }
-    }
-    */
-
-    private PlayerInput playerInput;
-
-    private void Awake()
-    {
-        playerInput = GetComponent<PlayerInput>(); // PlayerInput 컴포넌트를 가져옵니다.
-        playerInput.SwitchCurrentActionMap("Player"); // "Player"는 InputActionAsset에 정의된 이름입니다.
+        */
     }
 
     private void OnDisable()
     {
-        playerInput.DeactivateInput(); // PlayerInput 비활성화
+        PlayerInput.DeactivateInput(); // PlayerInput 비활성화
     }
 
     void OnRun()
     {
-        if (Input.GetKeyUp(KeyCode.Space) || playerInput.currentActionMap["Run"].triggered)
+        if (Input.GetKeyUp(KeyCode.Space) || PlayerInput.currentActionMap["Run"].triggered)
         {
             //연타하면 속력이 붙음
             Speed += 20f;
@@ -92,6 +115,7 @@ public class RaceGameManager: MonoBehaviour
 
     void OnSkill()
     {
+        Player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, (float)(Speed * Time.deltaTime * 1.1));
         Debug.Log("스킬");
     }
 }
---
+++ Assets/Script/npcScript.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcScript : MonoBehaviour
+{
+    public GameObject Npc;
+    public float npcSpeed;
+    private RaceGameManager raceGameManager;
+    private float ingTime = 0f;
+    public float Distance;
+
+
+    private void Awake()
+    {
+        // Scene에서 RaceGameManager 스크립트를 찾아서 가져옵니다.
+        raceGameManager = FindObjectOfType<RaceGameManager>();
+
+        RaceGameManager.instance.NpcZ = Npc.transform.position.z;
+        Distance = RaceGameManager.instance.PlayerZ - RaceGameManager.instance.NpcZ;
+    }
+
+    private void Update()
+    {
+        NpcGo();
+        NpcDefence();
+    }
+
+    void NpcRun()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) || raceGameManager.PlayerInput.currentActionMap["Run"].triggered)
+        {
+            
+            int randomValue = Random.Range(18, 26); // 18 이상 25 이하의 랜덤한 정수값을 생성합니다.
+            npcSpeed += randomValue;
+
+            Npc.GetComponent<Rigidbody>().AddForce(0, 0, npcSpeed * Time.deltaTime * 50);
+        }
+        else
+        {
+            //연타멈추면 속력이 줄어들음
+            Npc.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, npcSpeed * Time.deltaTime);
+        }
+
+        if (npcSpeed >= 800f)
+        {
+            npcSpeed = Random.Range(780, 800);
+        }
+
+        //뒤로 안가게 하기위한 처리
+        npcSpeed -= (0.5f * Time.deltaTime) * 80;
+
+        if (npcSpeed <= 0.0f)
+        {
+            npcSpeed = 0;
+        }
+    }
+
+    void NpcGo()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) || raceGameManager.PlayerInput.currentActionMap["Run"].triggered)
+        {
+            npcSpeed = RaceGameManager.instance.Speed * Random.Range(95, 110) / 100;
+            Npc.GetComponent<Rigidbody>().AddForce(0, 0, npcSpeed * Time.deltaTime * 50);
+
+            Debug.Log(RaceGameManager.instance.Speed + "그리고" + npcSpeed);
+        }
+        if (Distance > 5f)
+        {
+            while (Distance > 5f)
+            {
+                npcSpeed = 2000;
+            }
+
+            while (Distance < 5f)
+            {
+                npcSpeed = 0;
+            }
+        }
+    }
+
+    void NpcDefence()
+    {
+        if (raceGameManager.PlayerInput.currentActionMap["Skill"].triggered)
+        {
+            Debug.Log("수비");
+            ingTime += Time.deltaTime; // 경과 시간 누적
+
+            Npc.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, (float)(npcSpeed * Time.deltaTime * 0.8));
+        }
+    }
+}
